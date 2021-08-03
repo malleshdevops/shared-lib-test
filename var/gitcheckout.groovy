d6d640303b2bfd3b26224702576027cd54ab1a42
malleshdevops <81004732+malleshdevops@users.noreply.github.com>
2021-08-03 20:01:09 +0530

    Delete var directory

--- var/gitcheckout.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map stageParams) {
-checkout([
-  $class: 'GitSCM', 
-  branches: [[name: stageParams.branch]],
-  extensions: [], 
-  userRemoteConfigs: [[credentialsId: stageParams.credentials, 
-                url: stageParams.url]]])
-}
